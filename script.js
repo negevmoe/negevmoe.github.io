a8691d9ccfd65b82fa111c113bb83f44e41d0284
negevmoe <negevmoe@gmail.com>
2023-10-14 15:22:17 +0800

    fix: file request async

--- script.js
+++ script.js
@@ -2,29 +2,35 @@ var frame = 1; // 当前帧数
 var total = 839; // 总帧数
 
 var arr = []
+var load = []
 
-function pushFile() {
+function loading() {
     elem = document.getElementById("bar");
 
     for (let i = 1; i < total + 1; i++) {
-        $.get(`./ascii/ASCII-${i}.txt`, function (data) {
+        // 将读取文件请求添加到数组中
+        load.push($.get(`./ascii/ASCII-${i}.txt`, function (data) {
             arr.push(data)
-        });
+        }))
 
+        // 渲染进度条
         var width = Math.floor(i / total * 100)
         elem.style.width = width + '%';
         elem.innerHTML = `loading ${width}%`;
     }
 
-    elem.remove();
-    document.getElementById("progress").remove();
+    // 等待请求执行完毕
+    Promise.all(load).then(() => {
+        // 销毁进度条dom
+        elem.remove();
+        document.getElementById("progress").remove();
+
+        // 播放
+        intervalReader = setInterval(raed, 41);
+    })
 }
 
-function start_interval_reading(milliseconds) {
-    intervalReader = setInterval(FileReading, milliseconds);
-}
-
-function FileReading() {
-    frame = (frame % total) + 1;
-    document.getElementById("player").innerHTML = arr[frame];
+function raed(values) {
+    frame = (frame % total) + 1; // 计算当前帧数
+    document.getElementById("player").innerHTML = arr[frame]; // 渲染html内容
 }
