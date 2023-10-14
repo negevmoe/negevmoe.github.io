9ad776f329f055454ce878d65a3cc2bac6452080
negevmoe <negevmoe@gmail.com>
2023-10-14 15:41:17 +0800

    feat: promise

--- script.js
+++ script.js
@@ -3,31 +3,44 @@ var total = 839; // 总帧数
 
 var arr = []
 var load = []
+var elem
+
+window.onload = function () {
+    elem = document.getElementById("bar");
+    loading();
+}
 
 function loading() {
-    elem = document.getElementById("bar");
-
-    for (let i = 1; i < total + 1; i++) {
-        // 将读取文件请求添加到数组中
-        load.push($.get(`./ascii/ASCII-${i}.txt`, function (data) {
-            arr.push(data)
-
-            // 渲染进度条
-            var width = Math.floor(i / total * 100)
-            elem.style.width = width + '%';
-            elem.innerHTML = `loading ${width}%`;
-        }))
-    }
-
-    // 等待请求执行完毕
-    Promise.all(load).then(() => {
+    Promise.all(createPromiseArr()).then((values) => {
+        arr = values;
         // 销毁进度条dom
         elem.remove();
         document.getElementById("progress").remove();
 
         // 播放
         intervalReader = setInterval(raed, 41);
-    })
+    });
+}
+
+function createPromiseArr() {
+    let promise_list = [];
+
+    for (let i = 1; i < total; i++) {
+
+        let promise = new Promise((res, rej) => {
+            $.get(`./ascii/ASCII-${i}.txt`, (data, status) => {
+                res(data)
+
+                // 渲染进度条
+                var width = Math.floor(i / total * 100)
+                elem.style.width = width + '%';
+                elem.innerHTML = `loading ${width}%`;
+            });
+        });
+
+        promise_list.push(promise);
+    }
+    return promise_list;
 }
 
 function raed(values) {
